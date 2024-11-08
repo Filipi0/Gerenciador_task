6f8356d6d5efafad0a670c847465effad4ab2ecf
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2024-11-08 08:58:37 -0300

    tela de login

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,26 +1,16 @@
+<!-- src/App.vue -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <LoginPage />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './components/LoginPage.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    LoginPage
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ my-app/src/components/LoginPage.vue
@@ -0,0 +1,63 @@
+<!-- src/components/Login.vue -->
+<template>
+  <div class="login">
+    <h2>Login</h2>
+    <form>
+      <label for="username">Username:</label>
+      <input type="text" id="username" v-model="username" />
+      
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="password" />
+      
+      <button @click.prevent="login">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      // Lógica de autenticação aqui
+      console.log('Usuário:', this.username);
+      console.log('Senha:', this.password);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login {
+  max-width: 300px;
+  margin: auto;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+label {
+  display: block;
+  margin: 0.5rem 0 0.25rem;
+}
+
+input {
+  width: 100%;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+}
+
+button {
+  width: 100%;
+  padding: 0.5rem;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+}
+</style>
